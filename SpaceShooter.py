6686e8cca5e71fff378a169623b2034cdf1c4e1f
Manpreet <manpreet@bhatti.net>
2019-09-21 22:18:37 -0400

    Fix

--- SpaceShooter.py
+++
@@ -1,3 +0,0 @@
-from source.main import run
-
-run()
--- source/main.py
+++ source/main.py
@@ -1,10 +1,10 @@
 from PyGE.Engine import side_scroller
-from source.Objects.Bullet import Bullet
-from source.Objects.Enemy import Enemy, EnemyHandler
-from source.Objects.Player import Player
+from source.Objects.Skittle import Skittle
+from source.Objects.Pumpkin import Pumpkin, PumpkinHandler
+from source.Objects.Ghost import Ghost
 from source.Objects.Button import Button
 from source.Objects.Background import BackgroundImage
-from source.Objects.Enemy2 import Enemy2, EnemyHandler2
+from source.Objects.DoubleStackPumpkin import DoubleStackPumpkin, DoubleStackPumpkinHandler
 from source.Objects.Menu import MenuImage
 
 
@@ -13,11 +13,10 @@ def run():
         xml=open("resources/xml/rooms.xml").read(),
         start_room="menu",
         images={
-            "player1": {"path": "resources/images/player/player1.png", "w": 32, "h": 32},
-            "player2": {"path": "resources/images/player/player2.png", "w": 32, "h": 32},
-            "enemy": {"path": "resources/images/enemy/enemy1.png", "w": 32, "h": 32},
-            "enemy2": {"path": "resources/images/enemy/enemy2.png", "w": 32, "h": 32},
-            "bullet": {"path": "resources/images/bullets/bullet.png", "w": 32, "h": 32},
+            "ghost": {"path": "resources/images/ghost/ghost.png", "w": 32, "h": 32},
+            "pumpkin": {"path": "resources/images/pumpkins/pumpkin.png", "w": 32, "h": 32},
+            "doublestackpumpkin": {"path": "resources/images/pumpkins/doublestackpumpkin.png", "w": 32, "h": 32},
+            "skittle": {"path": "resources/images/skittles/skittle.png", "w": 32, "h": 32},
             "background": {"path": "resources/images/background/background.jpg", "w": 800, "h": 500},
             "menu": {"path": "resources/images/background/menu.jpg", "w": 800, "h": 500}
         },
@@ -30,7 +29,7 @@ def run():
           "Querround16": {"path": "resources/font/halloween/halloween.TTF", "size": 16}
         },
         custom_objects=[
-            Player, Bullet, Enemy, EnemyHandler, Button, BackgroundImage, Enemy2, EnemyHandler2, MenuImage
+            Ghost, Skittle, Pumpkin, PumpkinHandler, Button, BackgroundImage, DoubleStackPumpkin, DoubleStackPumpkinHandler, MenuImage
         ],
         initial_variables={
         },
